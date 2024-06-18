4af6e4b276a7b07e58cb3fb352334e7fae8e8e4c
lillmao69 <157451782+lillmao69@users.noreply.github.com>
2024-06-18 14:25:47 +0300

    Add files via upload

---
+++ Лабораторная работа 8.4/Задача1.py
@@ -0,0 +1,8 @@
+import random as rnd
+
+answers = ['Норм.', 'Лучше всех :)', 'Ну так', 'Отличненько!', 'Ничего, жить буду']
+
+def how_are_you():
+    return rnd.choice(answers)
+
+print(how_are_you())
